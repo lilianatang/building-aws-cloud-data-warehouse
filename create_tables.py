38e4ac5cae2713901efbe7edfe3b61bae8e97360
Liliana Tang <35161494+lilianatang@users.noreply.github.com>
2021-07-01 12:43:54 -0600

    provided documentation for functions in etl.py and create_tables.py

--- create_tables.py
+++ create_tables.py
@@ -4,12 +4,18 @@ from sql_queries import create_table_queries, drop_table_queries
 
 
 def drop_tables(cur, conn):
+    """
+    drops tables in Redshift.
+    """
     for query in drop_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def create_tables(cur, conn):
+    """
+    creates tables in Redshift.
+    """
     for query in create_table_queries:
         cur.execute(query)
         conn.commit()
--- etl.py
+++ etl.py
@@ -4,12 +4,18 @@ from sql_queries import copy_table_queries, insert_table_queries
 
 
 def load_staging_tables(cur, conn):
+    """
+    loads data from S3 to staging tables on Redshift.
+    """
     for query in copy_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def insert_tables(cur, conn):
+    """
+    loads data from staging tables to analytics tables on Redshift.
+    """
     for query in insert_table_queries:
         cur.execute(query)
         conn.commit()
